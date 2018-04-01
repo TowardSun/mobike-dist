427a4fb5747ad94860f31b1452e9e3c1d7de4319
liuzhaoyang <towardsun@sjtu.edu.cn>
2018-04-01 09:42:09 +0800

    commit run results

--- run.sh
+++ run.sh
@@ -11,10 +11,13 @@
 # nohup python run.py --train_cities bj --test_cities sh --model_choice 1 --feature_choice 0 --epochs 200 --gpu 1 > ./logs/bj_sh_dense_cnn.log 2>&1 &
 
 # multi-source feature test
-nohup python run.py --train_cities bj --test_cities nb --model_choice 0 --feature_choice 1 --epochs 200 --gpu 1 > ./logs/bj_nb_cnn_f1.log 2>&1 &
-nohup python run.py --train_cities bj --test_cities nb --model_choice 0 --feature_choice 2 --epochs 200 --gpu 1 > ./logs/bj_nb_cnn_f2.log 2>&1 &
-nohup python run.py --train_cities bj --test_cities nb --model_choice 0 --feature_choice 3 --epochs 200 --gpu 1 > ./logs/bj_nb_cnn_f3.log 2>&1 &
+nohup python run.py --train_cities sh --test_cities nb --model_choice 0 --feature_choice 1 --epochs 200 --gpu 0 > ./logs/bj_nb_cnn_f1.log 2>&1 &
+nohup python run.py --train_cities sh --test_cities nb --model_choice 0 --feature_choice 2 --epochs 200 --gpu 0 > ./logs/bj_nb_cnn_f2.log 2>&1 &
+nohup python run.py --train_cities sh --test_cities nb --model_choice 0 --feature_choice 3 --epochs 200 --gpu 0 > ./logs/bj_nb_cnn_f3.log 2>&1 &
 
+nohup python run.py --train_cities sh --test_cities bj --model_choice 0 --feature_choice 1 --epochs 200 --gpu 1 > ./logs/bj_nb_cnn_f1.log 2>&1 &
+nohup python run.py --train_cities sh --test_cities bj --model_choice 0 --feature_choice 2 --epochs 200 --gpu 1 > ./logs/bj_nb_cnn_f2.log 2>&1 &
+nohup python run.py --train_cities sh --test_cities bj --model_choice 0 --feature_choice 3 --epochs 200 --gpu 1 > ./logs/bj_nb_cnn_f3.log 2>&1 &
 
 # multi-source city
 # nohup python run.py --train_cities sh,bj --test_cities nb --model_choice 0 --feature_choice 0 --epochs 200 > ./logs/sh_bj_nb_cnn.log 2>&1 &
